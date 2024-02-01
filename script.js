8ac69ce657f5c3673aa95cfa777faab953e0d2b4
Kathy Quach <kathyquach@MacBook-Air.attlocal.net>
2024-01-31 21:51:14 -0800

    show suggestions function

--- script.js
+++ script.js
@@ -3,31 +3,38 @@ const suggestions = document.querySelector('.suggestions ul');
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
+let inputWord = "";
+
 function search(str) {
 	let results = [];
-	let newStr = str.toLowerCase();
-	const word = newStr.length > 1 ? newStr.split("") : (results = fruit.filter((fru) => fru.indexOf(newStr) !== -1));
-	for (let char of word){
-		results.push(fruit.filter((fru) => fru.indexOf(char) !== -1));
-		console.log(results)
-	}
-	results = new Set(results);
-	return results;
+	results = fruit.filter((fru) => fru.toLowerCase().includes(str));
+	console.log(results);
+	return results
 }
+
+
 
 function searchHandler(e) {
 	// TODO
+	inputWord += e.key.toLowerCase();
+	search(inputWord);
 }
 
 function showSuggestions(results, inputVal) {
-
 	// TODO
+	inputVal = search;
+	results = searchHandler;
+	for (let item of results){
+		let newSuggest = document.createElement("option");
+		newSuggest.innerText = item;
+		suggestions.appendChild(newSuggest);
+	}
+	console.log(suggestions);
 }
 
 function useSuggestion(e) {
 	// TODO
 }
 
-// input.addEventListener('keyup', searchHandler);
+input.addEventListener('keyup', searchHandler);
 suggestions.addEventListener('click', useSuggestion);
-input.addEventListener('keyup', search);
